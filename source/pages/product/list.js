8a5ba0395f272724ed30afa65cc5083b8dc4928b
Steve Cai <steve.cai@morningstar.com>
2017-12-08 18:19:32 +0800

    Commit for list NND搞了一天没搞完

--- source/pages/product/list.js
+++ source/pages/product/list.js
@@ -1,5 +1,6 @@
 // pages/product/list.js
-var MemberMgr=require("../../classes/MemberMgr.js");
+var MemberMgr = require("../../classes/MemberMgr.js");
+var Util = require("../../utils/util.js");
 Page({
   /**
    * 页面的初始数据
@@ -29,29 +30,45 @@ Page({
 
     var items=[];
     items.push({
+      id:1,
       img:"../../images/product.png",
       title:"爱国者超薄便携通用乌龙奶盖茶",
       price: 9999.00,
       oriprice:14100,
-      "type":"going",
-      reminder: 20,
+      "type": "going",
+      allstore: 20,
+      reminder: 7,
       starttime: "2017-12-10",
-      endtime:"2017-12-31"
+      endtime: "2017-12-31",
+      starttime_s:{hour:"0000",minute:"00",second:"00"},
+      endtime_s: { hour: "0000", minute: "00", second: "00" }
     });
     items.push({
+      id: 2,
       img: "../../images/product.png",
       title: "爱国者超薄便携通用乌龙奶盖茶",
       price: 9999.00,
       oriprice: 14100,
       "type": "coming",
+      allstore: 20,
       reminder: 20,
       starttime: "2017-12-10",
-      endtime: "2017-12-31"
+      endtime: "2017-12-31",
+      starttime_s: { hour: "0000", minute: "00", second: "00" },
+      endtime_s: { hour: "0000", minute: "00", second: "00" }
     });
 
     that.setData({ member: member,items:items });
+    this.goroundtimer=setInterval(function(){
+      var items=that.data.items;
+      for(var i=0;i<items.length;i++){
+        items[i].starttime_s = Util.timecutting(items[i].starttime);
+        items[i].endtime_s = Util.timecutting(items[i].endtime);
+      }
+      that.setData({items:items});
+    },1000);
   },
-
+  goroundtimer:null,
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
--- source/utils/util.js
+++ source/utils/util.js
@@ -13,7 +13,27 @@ const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
+const timecutting=n=>{
+  //console.log(n);
+  var date=new Date(n);
+  var second = (date.getTime() - (new Date()).getTime())/1000;
+  var hour=parseInt( second/3600);
+  second=second-3600*hour;
+  var minute = parseInt(second/60);
+  second = parseInt(second-minute*60);
+  hour = hour > 999 ? hour.toString() 
+  : (hour > 99?"0"+hour.toString()
+  :hour>9?"00"+hour.toString()
+        : (hour > 0 ? "000" + hour.toString() : "0000"));
+
+  minute = minute > 9 ? minute.toString() : "0" + minute.toString();
+  second = second > 9 ? second.toString() : "0" + second.toString();
+
+  //console.log(hour.toString() + "-"+minute.toString() + "-"+second.toString() );
+  return {hour:hour,minute:minute,second:second};
+}
 
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  timecutting: timecutting
 }
