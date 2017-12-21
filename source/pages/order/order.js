e4a5f3d59a2940be9a474ee40451b46509fb4b77
steve.cai <alucard263096@126.com>
2017-12-22 00:18:02 +0800

    修改目录结果

--- source/pages/order/order.js
+++ source/pages/order/order.js
@@ -1,121 +1,130 @@
 // pages/product/list.js
 var MemberMgr = require("../utils/classes/MemberMgr.js");
 var ProductMgr = require("../utils/classes/ProductMgr.js");
-var StorageMgr = require("../utils/classes/StorageMgr.js");
 var MerchantMgr = require("../utils/classes/MerchantMgr.js");
 var Util = require("../utils/utils/util.js");
 
 
-var KanproductApi=require('../utils/apis/kanproduct.js');
-var kanproductApi=new KanproductApi();
 
+var KanproductApi = require('../utils/apis/kanproduct.js');
+var kanproductApi = new KanproductApi();
+
+var KanorderApi = require('../utils/apis/kanorder.js');
+var kanorderApi = new KanorderApi();
 
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    "type":"all",
-    member:{},
-    displaytype:"",
-    items:[],
-    activeitem:0,
-    bordercase:{},
+    "type": "mycut",
+    cuttype: "cut_doing",
+    ordertype:"order_all",
+    member: {},
+    displaytype: "",
+    items: [],
+    activeitem: 0,
+    bordercase: {},
+    items_p: [],
+    items_finish: [],
+    items_myhelp: [],
+    items_all: []
   },
-  setLs(){
+  setLs() {
     wx.setStorage({
       key: 'ls',
       data: 'hellop',
     });
   },
-  changeType(e){
-    var t=e.currentTarget.id;
+  changeType(e) {
+    var t = e.currentTarget.id;
     this.setData({ "type": t });
     this.count();
+
+    // To Do:
+     
   },
-  changeDisplayType(e){
-    var displaytype = this.data.displaytype=="list"?"grid":"list";
-    var storageMgr = new StorageMgr();
-    storageMgr.setValue("list_displaytype", displaytype);
-    this.setData({ displaytype: displaytype});
-  },
-  gotoDetail(e){
-    var id=e.currentTarget.id;
+  goKanjia(e){
     wx.navigateTo({
-      url: '../productdetail/productdetail?id='+id,
+      url: '../productkanjia/productkanjia?id='+e.currentTarget.id,
     })
   },
-  gotoMyOrder(){
+  changeCutType(e){
+    var t = e.currentTarget.id;
+    this.setData({ "cuttype": t });
+    // To Do:
+
+  }, 
+  changeOrderType(e){
+    var t = e.currentTarget.id;
+    this.setData({ "ordertype": t });
+    // To Do:
+    
+  },
+  gotoDetail(e) {
+    var id = e.currentTarget.id;
     wx.navigateTo({
-      url: '../order/order',
+      url: '../productdetail/productdetail?id=' + id,
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that=this;
-    var member=MemberMgr.getMember(1);
-
-    var storageMgr=new StorageMgr();
-    var displaytype = storageMgr.getValue("list_displaytype");
-    console.log(displaytype);
-    if(displaytype==""){
-      displaytype="list";
-    }
+    var that = this;
+    var member = MemberMgr.getMember();
     var app_id = MerchantMgr.getAppId();
-    kanproductApi.list({ zhichiapp_id: app_id,orderby:"seq desc,starttime"},function(data){
-      var items = data;
-      console.log(items);
-      that.setData({ items: items});
-      that.count(true);
-      that.goroundtimer = setInterval(function () {
-        that.count();
-      }, 1000);
-    });
-    
-    var broadcase = ProductMgr.getProductKanjiaBroadcase();
 
-    that.setData({ member: member, displaytype: displaytype, broadcase: broadcase});
+    kanorderApi.progress({member_id:member.id,zhichiapp_id:app_id},function(data){
+      that.setData({items_p:data});
+    });
+    kanorderApi.finish({ member_id: member.id, zhichiapp_id: app_id }, function (data) {
+      that.setData({ items_finish:data });
+    });
+    kanorderApi.myhelp({ member_id: member.id, zhichiapp_id: app_id }, function (data) {
+      that.setData({ items_myhelp: data });
+    });
+    kanorderApi.myhelp({ member_id: member.id, zhichiapp_id: app_id }, function (data)    {
+      that.setData({ items_all: data });
+    });
+
+
+    that.setData({ member: member });
   },
-  count(amountcut=false){
-    var that=this;
+  count() {
+    var that = this;
     var items = that.data.items;
     var ty = that.data.type;
     var activeitem = 0;
     for (var i = 0; i < items.length; i++) {
-      if (amountcut){
-        items[i].lowprice_str = Util.amountcutting(items[i].lowprice);
-      }
       items[i].starttime_s = Util.timecutting(items[i].starttime);
       items[i].endtime_s = Util.timecutting(items[i].endtime);
       if ((items[i].endtime_s.reminder > 0) && (ty == 'all' || (ty == 'going' && items[i].starttime_s.reminder < 0) || (ty == 'coming' && items[i].starttime_s.reminder > 0))) {
         activeitem++;
       }
     }
-    //console.log(items);
     that.setData({ items: items, activeitem: activeitem });
   },
-  goroundtimer:null,
+  goroundtimer: null,
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
@@ -129,20 +138,20 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
---
+++ source/pages/utils/utils/util.js
@@ -0,0 +1,46 @@
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+const timecutting=n=>{
+  //console.log(n);
+  var date=new Date(n);
+  var second = (date.getTime() - (new Date()).getTime())/1000;
+  var reminder=second;
+  var hour=parseInt( second/3600);
+  second=second-3600*hour;
+  var minute = parseInt(second/60);
+  second = parseInt(second-minute*60);
+  hour = hour > 999 ? hour.toString() 
+  : (hour > 99?"0"+hour.toString()
+  :hour>9?"00"+hour.toString()
+        : (hour > 0 ? "000" + hour.toString() : "0000"));
+
+  minute = minute > 9 ? minute.toString() : "0" + minute.toString();
+  second = second > 9 ? second.toString() : "0" + second.toString();
+
+  //console.log(hour.toString() + "-"+minute.toString() + "-"+second.toString() );
+  return {hour:hour,minute:minute,second:second,reminder:reminder};
+}
+const amountcutting=n=>{
+  var b=Number(n).toFixed(0);
+  var s ="."+ Number(n).toFixed(2).split(".")[1].substring(0,2);
+  return {b:b,s:s}
+}
+
+module.exports = {
+  formatTime: formatTime,
+  timecutting: timecutting,
+  amountcutting: amountcutting
+}
